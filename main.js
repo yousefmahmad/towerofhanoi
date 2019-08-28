94d2667eb9d5f0ce9e7fb0be33ed50c1c5ba61d0
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-08-28 14:26:43 -0400

    sort of working

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const three = document.querySelector('#three')
 // create an array with each one of the disks within it
 let disks = [one, two, three]
 
+
 // create a variable for each stack
 const stackA = document.querySelector('#A')
 const stackC = document.querySelector('#C')
@@ -15,22 +16,33 @@ const StackB = document.querySelector('#B')
 // maybe if i add
 // Moving the blocks from one container to the other
 // ran into bug that when one block is clicked, they all go to stack that corresponds with their number
+
+
 function selectDisk() {
   for (let i = 0; i < disks.length; i++)
     disks[i].addEventListener('click', function () {
-     disks[i].style.border = 'Red 2px Solid'
+     disks[i].style.border = 'Red 2px Solid';
+     console.log(disks[i]);
+     stackA.addEventListener('click', function(){
+      stackA.appendChild(disks[i])});
+     stackB.addEventListener('click', function(){
+      stackB.appendChild(disks[i])});
+      stackC.addEventListener('click', function(){
+        stackC.appendChild(disks[i])});
+      })
     }
-    )
-}
+    
 console.log(selectDisk())
 
-function selectStack() {
-  for (let i = 0; i < stack.length; i++)
-    stack[i].addEventListener('click', function () {
-      stack[i].style.backgroundColor = 'pink'
-    })
-}
-console.log(selectStack())
+// Moves the disks together into the different container
+// function selectStack1() {
+//   for (let i = 0; i < stack.length; i++)
+//     stack[i].addEventListener('click', function () {
+//       stack[i].appendChild(disks[0])
+//     })
+// }
+// console.log(selectStack1())
+
 
   // *create a solution set to set your winning parameter to in order to compare it to something else. Need to figure out how to get the computer
   // to read that each individual disk is ontop of the other, so that it will read the disks from top to bottom, comparing their widths
