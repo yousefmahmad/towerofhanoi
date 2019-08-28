fcd172651bc2c3215d8966bae68934d7186fcdad
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-08-28 11:26:31 -0400

    made one event listener work

--- main.js
+++ main.js
@@ -12,30 +12,25 @@ const stackA = document.querySelector('#A')
 const stackC = document.querySelector('#C')
 const StackB = document.querySelector('#B')
 
-//* create a function that allows you to move each disk to the top of the 
-// of the stack, unless the disk trying to be placed ontop of the other disk is 
-// larger than the one below it
-// function compare() {
-//   if (destination.firstChildElement < 
-//   }
-
-// add event listener on the container that will then store the elements after clicked upon
-function start(){
-  console.log('Play')
-  for( let i = 0; i < stack.length; i++)
-    stack[i].addEventListener('click', storeElements)
-}
-
+// maybe if i add
 // Moving the blocks from one container to the other
-function move(){
-    disks.addEventListener('click', function(evt){
-      evt.preventDefault();
-      for(let i = 0; i < disks.length; i++){
-      evt.target.style.border = '1px solid red'
-      } 
+// ran into bug that when one block is clicked, they all go to stack that corresponds with their number
+function selectDisk() {
+  for (let i = 0; i < disks.length; i++)
+    disks[i].addEventListener('click', function () {
+     disks[i].style.border = 'Red 2px Solid'
+    }
+    )
+}
+console.log(selectDisk())
+
+function selectStack() {
+  for (let i = 0; i < stack.length; i++)
+    stack[i].addEventListener('click', function () {
+      stack[i].style.backgroundColor = 'pink'
     })
 }
-console.log(move())
+console.log(selectStack())
 
   // *create a solution set to set your winning parameter to in order to compare it to something else. Need to figure out how to get the computer
   // to read that each individual disk is ontop of the other, so that it will read the disks from top to bottom, comparing their widths
@@ -44,4 +39,4 @@ console.log(move())
   // Test this out once you're able to start moving the disks around to see how the computer pushes each move into an array
 
   // basically the solution is to have the disk divs as they are, except in stack C
-  // const solution = 
+  // const solution 
