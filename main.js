2cdf7fed634360913de298f65c94f21147a3e92b
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-08-30 11:24:08 -0400

    progress

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const stackB = document.querySelector('#B')
 const stackC = document.querySelector('#C')
 
 // create a variable for all the stacks
-const stacks = [stackA, stackB, stackC]
+let stacks = [stackA, stackB, stackC]
 //* create variables for each disk
 const one = document.querySelector('#one')
 const two = document.querySelector('#two')
@@ -16,32 +16,47 @@ let disks = [one, two, three]
 
 let activeDisk = null
 let activeStack = null
-let game = document.querySelector('.game')
 
 // Moving the blocks from one container to the other
-
-function move() {
-    game.addEventListener('click', function(evt){
-      // activeDisk = evt.target.dataset.value
-      // activeStack = evt.target.parentElement.id
-
-      // console.log(activeDisk)
-      console.log(move())
-    })
-      
-
-    // if (activeStack.id === one) {
-    //   stackB.addEventListener('click', function(){
-    //     stackB.prepend(activeDisk)}
-    //     )
-
-    //     &&
-
-    //     stackC.addEventListener('click', function(){
-    //       stackC.prepend(activeDisk)}
-    //       )
-    //   } 
+for (let i = 0; i < disks.length; i++) {
+  // disks[i].addEventListener('click', move.bind(evt))
+  disks[i].addEventListener('click', move)
 }
+function move(evt) {
+  console.log(evt)
+  stacks = [stackA, stackB, stackC]
+  activeDisk = evt.target.dataset.value
+  activeStack = evt.target.parentElement.id
+  console.log(activeDisk)
+  console.log(activeStack)
+  // give red border on block when selected
+
+  // Go through all stacks and remove active stack from array
+  for (let i = 0; i < stacks.length; i++) {
+    if (activeStack === stacks[i].id) {
+      stacks.splice(i, 1);
+      console.log(stacks)
+    }
+  }
+
+  // Put event listeners on remaining stacks
+  for (let j = 0; j < stacks.length; j++) {
+    stacks[j].addEventListener('click', check)
+  }
+}
+
+
+
+function check(evt) {
+  if (evt.target.firstElementChild > activeDisk) {
+    stacks[j].prepend(activeDisk)
+    console.log(evt)
+  }
+
+}
+
+
+
 
 
 
